f95e201296eb4ec63b267f7be4a4506ebbca470e
Anthony Bailly <Anthony-bailly@live.fr>
2015-11-23 16:04:45 +0100

    Update MainTest.java

--- Work01/src/backup/Anthony/MainTest.java
+++ Work01/src/backup/Anthony/MainTest.java
@@ -11,5 +11,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
+  private Main sut = new Main();
+  private source1 = ":F\Users\Bla\Workspace\Work01\Source1"
+  private source2 = ":F\Users\Bla\Workspace\Work01\Source2"
+  
+  @test
+  public void recursiveDir()
+  {
+    //TreeMap<String,List<String>> res = new TreeMap<String, List<String>>();
+    //recursivescanDir(source1,res);
+    //Assert.assertEquals(":F\Users\WorkSpace\Work01\Source1\C.txt",res)
+  }
 
 }
